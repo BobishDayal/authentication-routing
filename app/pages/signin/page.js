35bb3a61c5613e933f7322113f8cb2672c6694ad
bobish dayal <bobishdayal@gmail.com>
2023-10-17 17:26:12 +0545

    sign up in progress

--- app/pages/signin/page.js
+++ app/pages/signin/page.js
@@ -29,9 +29,9 @@ const SignIn = () => {
 
   return (
     <Wrapper>
-      <div className=" w-96 h-3/5 border-2 rounded-2xl border-transparent bg-white/[.1]">
-        <h4 className="font-extrabold text-4xl text-gray-50 m-7 text-center">
-          Sign In
+      <div className=" w-2/4 h-3/5 border-4 rounded-2xl border-red-600/[.5] bg-black/[.2]">
+        <h4 className="font-extrabold text-4xl text-white m-7 text-center">
+          Sign Up
         </h4>
         <form
           className="flex flex-col w-4/5 h-4/5 items-center justify-center mx-auto "
@@ -42,7 +42,7 @@ const SignIn = () => {
             type="text"
             autoComplete="on"
             placeholder="First Name"
-            className="border-2 border-gray-300 rounded-2xl p-2 m-2 w-full bg-gray-300 placeholder:text-gray-400 placeholder:font-bold"
+            className="border-2 border-gray-300 rounded-2xl p-2 m-2 w-full bg-transparent placeholder:text-gray-400 placeholder:font-bold"
             ref={firstnameInputRef}
           />
           <input
@@ -50,7 +50,7 @@ const SignIn = () => {
             type="text"
             autoComplete="on"
             placeholder="Last Name"
-            className="border-2 border-gray-300 rounded-2xl p-2 m-2  w-full  bg-gray-300  placeholder:text-gray-400 placeholder:font-bold"
+            className="border-2 border-gray-300 rounded-2xl p-2 m-2  w-full  bg-transparent  placeholder:text-gray-400 placeholder:font-bold"
             ref={lastnameInputRef}
           />
           <input
@@ -58,20 +58,20 @@ const SignIn = () => {
             type="email"
             autoComplete="on"
             placeholder="Email"
-            className="border-2 border-gray-300 rounded-2xl p-2 m-2  w-full  bg-gray-300  placeholder:text-gray-400 placeholder:font-bold"
+            className="border-2 border-gray-300 rounded-2xl p-2 m-2  w-full  bg-transparent  placeholder:text-gray-400 placeholder:font-bold"
             ref={emailInputRef}
           />
           <input
             id="password"
             type="password"
             placeholder="password"
-            className="border-2 border-gray-300 rounded-2xl p-2 m-2  w-full  bg-gray-300  placeholder:text-gray-400 placeholder:font-bold"
+            className="border-2 border-gray-300 rounded-2xl p-2 m-2  w-full  bg-transparent  placeholder:text-gray-400 placeholder:font-bold"
             ref={passwordInputRef}
           />
           <input
             type="submit"
-            value="Sign In"
-            className="border-transparent border-2 rounded-2xl bg-white text-blue-800 font-bold px-9 py-2 m-7 hover:bg-blue-800 hover:text-white cursor-pointer"
+            value="Sign Up"
+            className="border-transparent border-2 rounded-3xl bg-red-500 text-white font-bold px-6 py-2 m-7 hover:scale-110 ease-in duration-200 cursor-pointer"
           />
         </form>
       </div>
