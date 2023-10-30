59d00fc6c35c33a6094ff9cb39b2433ba19fc4fe
bobish dayal <bobishdayal@gmail.com>
2023-10-30 15:29:58 +0545

    contact page implemented

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -1,5 +1,43 @@
 const Contact = () => {
-  return <div className="h-screen bg-black w-full"></div>;
+  return (
+    <div
+      className=" w-full flex flex-col items-end   opacity-80"
+      style={{
+        backgroundImage: `url(/assets/pain.png)`,
+        backgroundSize: "56% 100%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "left",
+        height: "150vh",
+      }}
+    >
+      <h3 className="text-8xl font-extrabold text-red-500 mr-60 mt-16 mb-12">
+        Contact me
+      </h3>
+
+      <div className=" h-2/5 w-2/5 border-2 border-white/[.6] rounded-xl mx-36 relative flex flex-col items-end">
+        <h4 className="text-5xl font-extrabold text-white text-center my-9 mr-20 ">
+          Get in Touch
+        </h4>
+        <div className="w-60 h-80 bg-slate-300 absolute bottom-14 -left-16 rounded-lg shadow-custom"></div>
+        <form className=" mr-20  w-72">
+          <label className="text-white font-extrabold ">Name</label>
+          <br></br>
+          <input className="w-full h-10 rounded-lg my-1 mb-4 bg-slate-400/[.3] border-2 border-white/[.2]" />
+          <br></br>
+          <label className="text-white font-bold">Your Message</label>
+          <br></br>
+          <textarea className="w-full h-24 rounded-lg my-1 mb-4  bg-slate-400/[.3] border-2 border-white/[.2]" />
+          <br></br>
+          <button
+            type="submit"
+            className="rounded-lg px-4 py-1 font-extrabold bg-red-600 shadow-custom text-white hover:scale-110 ease-in duration-100"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- app/pages/gallery/page.js
+++ app/pages/gallery/page.js
@@ -4,17 +4,17 @@ import Data from "../../data";
 const Gallery = () => {
   return (
     <div
-      className=" w-full flex flex-col items-end justify-center my-36 opacity-80"
+      className=" w-full flex flex-col items-end justify-center  opacity-80"
       style={{
-        backgroundImage: `url(/assets/pain.png)`,
-        backgroundSize: "80% 100%",
+        backgroundImage: `url(/assets/akatsukiLogo.jpg)`,
+        backgroundSize: "100% 80%",
         backgroundRepeat: "no-repeat",
-        backgroundPosition: "left",
-        height: "230vh",
+        backgroundPosition: "center",
+        height: "140vh",
       }}
     >
-      <h3 className="text-8xl font-extrabold text-red-500 mx-auto">Gallery</h3>
-      <div className="grid grid-cols-2 gap-24 mx-32 mt-28">
+      <h3 className="text-8xl font-extrabold text-red-500 mx-auto">Members</h3>
+      <div className="grid grid-cols-5 gap-24 mx-auto mt-28">
         {Data.map((info) => {
           return <ImageContainer data={info} key={info.key} />;
         })}
