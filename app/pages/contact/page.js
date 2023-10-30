fa665d5c5501918918746c0c6f86fa994bbb3891
bobish dayal <bobishdayal@gmail.com>
2023-10-30 17:56:01 +0545

    ui update for contact page

--- app/pages/contact/page.js
+++ app/pages/contact/page.js
@@ -1,3 +1,12 @@
+import { BiLogoFacebookSquare, BiPhone } from "react-icons/bi";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+import { BiLogoTwitter } from "react-icons/bi";
+import { BiSolidPhone } from "react-icons/bi";
+import { BiLogoGmail } from "react-icons/bi";
+import { BiLogoGithub } from "react-icons/bi";
+import { HiMapPin } from "react-icons/hi2";
+
 const Contact = () => {
   return (
     <div
@@ -10,21 +19,58 @@ const Contact = () => {
         height: "150vh",
       }}
     >
-      <h3 className="text-8xl font-extrabold text-red-500 mr-60 mt-16 mb-12">
+      <h3 className="text-8xl font-extrabold text-red-800 mr-60 mt-16 mb-12">
         Contact me
       </h3>
 
       <div className=" h-2/5 w-2/5 border-2 border-white/[.6] rounded-xl mx-36 relative flex flex-col items-end">
-        <h4 className="text-5xl font-extrabold text-white text-center my-9 mr-20 ">
+        <h4 className="text-5xl font-extrabold text-gray-100 text-center my-9 mr-20 ">
           Get in Touch
         </h4>
-        <div className="w-60 h-80 bg-slate-300 absolute bottom-14 -left-16 rounded-lg shadow-custom"></div>
+        <div className="w-60 h-80 bg-red-800 absolute bottom-14 -left-16 rounded-lg shadow-custom flex flex-col justify-center">
+          <p className="font-extrabold text-lg mx-5 mb-5 text-white">
+            You can reach me on
+          </p>
+
+          <div className="flex flex-col mx-4 mb-7">
+            <div className="flex my-2 items-center">
+              <HiMapPin size={25} color="#333333" />
+              <p className="mx-4 text-xs font-semibold text-gray-300">
+                Kathmandu, Nepal
+              </p>
+            </div>
+            <div className="flex  items-center ">
+              <BiSolidPhone size={25} color="#333333" />
+              <p className="mx-4 text-xs font-semibold  text-gray-300">
+                +977 9765962655
+              </p>
+            </div>
+            <div className="flex my-2 items-center">
+              <BiLogoGmail size={25} color="#333333" />
+              <p className="mx-4 text-xs font-semibold  text-gray-300">
+                Bobishdayal@gmail.com
+              </p>
+            </div>
+            <div className="flex items-center">
+              <BiLogoLinkedin size={25} color="#333333" />
+              <p className="mx-4 text-xs font-semibold  text-gray-300">
+                efjfjjefjhje
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-evenly items-center">
+            <BiLogoFacebookSquare size={35} color="white" />
+            <BiLogoInstagramAlt size={35} color="white" />
+            <BiLogoGithub size={35} color="white" />
+            <BiLogoTwitter size={35} color="white" />
+          </div>
+        </div>
         <form className=" mr-20  w-72">
-          <label className="text-white font-extrabold ">Name</label>
+          <label className="text-gray-300 font-extrabold ">Name</label>
           <br></br>
           <input className="w-full h-10 rounded-lg my-1 mb-4 bg-slate-400/[.3] border-2 border-white/[.2]" />
           <br></br>
-          <label className="text-white font-bold">Your Message</label>
+          <label className="text-gray-300 font-bold">Your Message</label>
           <br></br>
           <textarea className="w-full h-24 rounded-lg my-1 mb-4  bg-slate-400/[.3] border-2 border-white/[.2]" />
           <br></br>
